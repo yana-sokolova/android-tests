a124cf3418a86e303c2a35699fa92d41eeabd2d3
Yana Sokolova <63470368+yana-sokolova@users.noreply.github.com>
2020-04-10 15:11:03 -0700

    Create browser_stack_web_upload.sh
    Upload via API from web location

---
+++ browser_stack_web_upload.sh
@@ -0,0 +1,1 @@
+curl -u "USERNAME:ACCESS_KEY" -X POST "https://api-cloud.browserstack.com/app-live/upload" -F "data={\"url\": \"https://www.browserstack.com/app-live/sample-apps/android/WikipediaSample.apk\"}"
